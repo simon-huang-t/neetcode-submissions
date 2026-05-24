fd53c833a4a9843b969c1bff556660050ffbf43e
simon-huang-t <72698744+simon-huang-t@users.noreply.github.com>
2026-05-24 14:04:48 +0200

    Add: surrounded-regions - submission-14

---
+++ Data Structures & Algorithms/surrounded-regions/submission-14.py
@@ -0,0 +1,35 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        m, n = len(board), len(board[0])
+
+        def capture():
+            directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+            queue = deque()
+
+            for r in range(m):
+                for c in range(n):
+                    if (r == 0 or r == m - 1 or c == 0 or c == n - 1) and board[r][c] == 'O':
+                        board[r][c] = 'S'
+                        queue.append((r, c))
+
+            while queue:
+                r, c = queue.popleft()
+
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+
+                    if 0 <= nr < m and 0 <= nc < n and board[nr][nc] == 'O':
+                        board[nr][nc] = 'S'
+                        queue.append((nr, nc))
+
+        capture()
+
+        for r in range(m):
+            for c in range(n):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+                elif board[r][c] == 'S':
+                    board[r][c] = 'O'
+        
+        
+        
