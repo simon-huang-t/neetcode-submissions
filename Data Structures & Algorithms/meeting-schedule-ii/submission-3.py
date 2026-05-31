48da6f592f7b4b8cb92c4851f15a8bbed956197b
simon-huang-t <72698744+simon-huang-t@users.noreply.github.com>
2026-05-31 14:32:20 +0200

    Add: meeting-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-3.py
@@ -0,0 +1,24 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        events = []
+        for interval in intervals:
+            events.append((interval.start, 'S'))
+            events.append((interval.end, 'E'))
+        events.sort()
+        overlapping = 0
+        res = 0
+        for event_time, event_type in events:
+            if event_type == 'S':
+                overlapping += 1
+            else:
+                overlapping -= 1
+            res = max(res, overlapping)
+        return res
